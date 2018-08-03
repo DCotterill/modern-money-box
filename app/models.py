4edf26fd25b8ad9e21e5252570080aca14c7ce61
Darren Cotterill <darren.cotterill@gmail.com>
2018-08-03 11:09:57 +1000

    Add User login screen

--- app/models.py
+++ app/models.py
@@ -1,11 +1,33 @@
+from datetime import datetime
 from app import db
+from app import login
+
+from flask_login import UserMixin
 
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
 
+    accounts = db.relationship('Account', backref='owner', lazy='dynamic')
+
     def __repr__(self):
         return '<User {}>'.format(self.username)
+
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+
+
+class Account(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    balance = db.Column(db.Integer)
+    name = db.Column(db.String(140))
+    created_date = db.Column(db.DateTime, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return '<Account {}>'.format(self.name)
